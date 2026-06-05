f99543d849a16ecabe1df9f47928a59ea52f4eb2
kifyy <21980592@students.latrobe.edu.au>
2026-06-05 12:05:42 +1000

    add firebase config

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,18 @@ import { ActivityIndicator, StyleSheet } from 'react-native';
 
 import { ThemedView } from '@/components/themed-view';
 import { useUserProfile } from '@/contexts/user-profile-context';
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import '../../firebase.js';
+const {firebaseConfig} = require('../../firebase.js');
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function IndexScreen() {
   const { isOnboardingComplete, isLoading } = useUserProfile();
